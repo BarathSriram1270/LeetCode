c04304055234a004a0c002a5e0093319b741a731
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2024-04-23 21:12:05 +0530

    Create binary-tree-inorder-traversal.java

---
+++ binary-tree-inorder-traversal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+        }
+        
+        return result;
+    }
+}
+
