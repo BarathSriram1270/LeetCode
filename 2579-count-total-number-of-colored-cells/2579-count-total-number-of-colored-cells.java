bef8a5f4f6ad05be81e6f6636066116de5801cb0
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2025-03-05 10:14:38 +0530

    Time: 0 ms (100%), Space: 40.3 MB (73.36%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,12 +1,5 @@
 class Solution {
     public long coloredCells(int n) {
-       long x=1;
-       long c=4;
-       while(n>1){
-        x+=c;
-        c+=4;
-        n--;
-       }
-       return x;
+      return 1+(long)2*n*(n-1);
     }
 }
