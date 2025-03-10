f7f074b6c42b63ec0d834df9b68142c51ca35d82
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2025-03-10 12:08:58 +0530

    Time: 0 ms (100%), Space: 40.7 MB (21.97%) - LeetHub

--- 0991-broken-calculator/0991-broken-calculator.java
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -1,32 +1,15 @@
 class Solution {
     public int brokenCalc(int st, int t) {
-        if (st==t) return 0;
-        if (st>t) return st-t;
-        int c=0;
-        while(st<t){
-            if(st*2==t){
-                st=st*2;
-                c++;
-            }
-            else if((st-1)*2==t){
-                st--;
-                st=st*2;
-                c+=2;
-            }
-            else if((st*2)-1 == t){
-                st*=2;
-                st--;
-                c+=2;
-            }
-            else if(st*2 < t){
-                st*=2;
-                c++;
+        int o=0;
+        while(t>st){
+            if(t%2==0) {
+                t/=2;
             }
             else{
-                st--;
-                c++;
+                t++;
             }
+            o++;
         }
-        return c;
+        return o+(st-t);
     }
 }
