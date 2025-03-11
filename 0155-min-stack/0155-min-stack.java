200364fa4a0ff380d2806f5a0f0edb9916745d58
Barathsriram <133561863+BarathSriram1270@users.noreply.github.com>
2025-03-11 15:16:35 +0530

    Time: 5 ms (38.16%), Space: 44.9 MB (67.69%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -5,12 +5,13 @@ class MinStack {
     private Stack<Integer> minStack=new Stack<>();
 
     public MinStack() {
-        
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
     
     public void push(int val) {
         stack.push(val);
-        if (minStack.isEmpty() || val <= minStack.peek()) {
+        if (minStack.isEmpty() || val < minStack.peek()) {
             minStack.push(val);
         } else {
             minStack.push(minStack.peek());
